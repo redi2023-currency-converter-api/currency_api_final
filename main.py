210222ffb6ff3f3fad1e6b9b1b28841366db911b
albanda89 <bandaraaruni899@gmail.com>
2023-07-08 10:48:32 +0200

    Improved error handling

--- main.py
+++ main.py
@@ -33,39 +33,25 @@ class CurrencyConverter:
 
         return amount, source_currency, target_currency
 
+    # calling the REST API to get the exchange rates. 
+    # All errors and exceptions are deligated to the caller
     def get_exchange_rate(self, source_currency, target_currency):
+
         params = {
             'apikey': config.API_KEY,
             'base_currency': source_currency,
         }
-
         response = requests.get(config.BASE_URL, params=params)
-        ### handling HTTP response headers. See https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-        # HTTP handling in API necessary documented in API documentation. See https://freecurrencyapi.com/docs/#authentication-methods
-        # no error handling but during the  get_exchange_rate , only in an error exceptions are thrown 
 
-        if response.status_code == 401:
-            raise Exception('API authentication failed. Invalid API key provided. Unable to access the API')
-
-        if response.status_code == 429:
-            raise Exception('API quota rate limit reached. API refused connection. Please wait')
-
-        if response.status_code != 200:
-            raise Exception('Error while connecting to the API. Giving up')
-
+        # throwing resulting errors to the caller
+        response.raise_for_status()
         data = response.json()
-
-        if 'data' not in data:
-            raise Exception('Error while fetching data from the API. No data received')
-
+        assert 'data' in data, 'Malformed json response from the REST API. The element \'data\' not found'
         rates = data['data']
-
-        if target_currency not in rates:
-            raise Exception(f"Provided 'target_currency' {target_currency} is not found in the API response")
 
         return rates[target_currency]
     
-    # Create a currency converter function
+    # Convert the currencies with help of the exchange rates feteched from the API
     def convert(self, user_input):
         # Create a variable that store the currency exchange change rate from the API
         exchange_rate = self.get_exchange_rate(user_input[1], user_input[2])
@@ -85,6 +71,19 @@ def main():
 
         # Print the output and limit the precision to 2 decimal places
         print(f"{user_input[0]} {user_input[1]} is equivalent to {round(output, 2)} {user_input[2]}.")
+    
+    except requests.exceptions.ConnectionError as e:
+        print('Network error. Unable to call the REST API: ', e)
+
+    except requests.exceptions.HTTPError as e:
+        print('The REST API returned an error: ', e)
+
+    except requests.exceptions.RequestException as e:
+        print('Error while connecting to the REST API:', e)
+    
+    except AssertionError as e:
+        print(e)
+
     except Exception as e:
         print('Unable to convert the currency. Program ran into an error:', e)
 
